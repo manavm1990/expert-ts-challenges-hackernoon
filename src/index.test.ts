c040d335cecb077edddf7e51cbf6d4fa39d3d5e4
Manav Misra <manavm@visionwebsoft.com>
2022-10-08 16:53:22 -0500

    feat: return '0' on empty string

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,10 +1,17 @@
-import { describe, expect, test } from 'vitest';
+import { it, describe, expect, test } from 'vitest';
 import { getLenOfLongestSubstringWithoutRepeatingChars } from '.';
 
 describe('getLenOfLongestSubstringWithoutRepeatingChars', () => {
   test('Find the length of the longest substring without repeating characters', () => {
     const input = 'abbbcabcdefef';
     const output = 6;
+
+    expect(getLenOfLongestSubstringWithoutRepeatingChars(input)).toBe(output);
+  });
+
+  it('should return 0 for empty string', () => {
+    const input = '';
+    const output = 0;
 
     expect(getLenOfLongestSubstringWithoutRepeatingChars(input)).toBe(output);
   });
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
-export const add2Nums = (a: number, b: number): number => a + b;
+export const getLenOfLongestSubstringWithoutRepeatingChars = (
+  str: string,
+): number => {
+  let longest = 0;
+  let current = 0;
+
+  const sub = new Set();
+
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i];
+
+    if (sub.has(char)) {
+      current = 0;
+      sub.clear();
+    }
+
+    sub.add(char);
+    current++;
+    longest = Math.max(longest, current);
+  }
+
+  return longest;
+};
